ebed8e64a3d726f299a7723e2b20b5d8be921390
RubensCosta22 <97482359+RubensCosta22@users.noreply.github.com>
2026-01-26 14:27:50 -0300

    Refactor service worker installation and activation
    Removed caching during service worker installation and added client claiming on activation.

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,14 @@
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("inspecto-v1").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./App.jsx",
-        "./main.jsx"
-      ]);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
+
